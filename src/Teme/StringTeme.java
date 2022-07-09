169eb80cba7997fb146f55c0fe0f2e1b0a4da013
MihaelaAntalute <antalutemihaela32@gmail.com>
2022-07-09 11:52:01 +0300

    string teme

---
+++ src/Teme/StringTeme.java
@@ -0,0 +1,22 @@
+package Teme;
+
+import java.util.Scanner;
+
+public class StringTeme {
+    public static void main(String[] args) {
+
+
+
+
+
+        //Ex.2 Afiseaza cate litere,numere,spatii si cifre sunt intr-un String
+        static void getCharCount(){
+            Scanner console= new Scanner(System.in);
+            System.out.println("Enter a sentences:");
+            String sentence = console.nextLine();
+
+
+
+        }
+    }
+}
